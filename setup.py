b2c9fd13b4238dee8537c99c57f02c8ffe6eb623
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-05-29 12:16:34 -0500

    Initial commit with minimal tox/pytest

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='evotrees',
+    version='0.1.0',
+    description='Fold linear revision histories into evolutionary trees',
+    url='https://github.com/evoapps/evotrees',
+    author='Pierce Edmiston',
+    author_email='pierce@evoapps.xyz',
+    packages=['evotrees'],
+)
